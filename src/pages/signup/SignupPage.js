6a9b9ac234322da4e181b0edff26e79c03b17b15
Weston Woodard <westonwoodard28@gmail.com>
2021-03-01 14:21:23 -0700

    stying complete. needs review

--- src/pages/signup/SignupPage.js
+++ src/pages/signup/SignupPage.js
@@ -29,6 +29,7 @@ const kf = keyframes`
 const Page = styled.div`
 font-family:'Roboto',sans-serif;`
 
+
 // banner
 const HeaderImg = styled.img`
     object-fit: cover;
@@ -40,10 +41,12 @@ const HeaderImg = styled.img`
 //entirety of sign up form
 const SignUpBox = styled.div`
     border: 2px solid black;
-    width: 80%;
+    width: 60%;
     margin: 2rem auto;
     opacity: 0;
     animation: ${kf} 1s ease-in-out forwards;
+    
+    
 `
 const SignUp = styled.h1`
     text-align:center;
@@ -60,42 +63,96 @@ const FirstName = styled.input`
     margin: 1rem;
     padding: .5rem;
     border: 4px dotted #c35b48;
+    &:focus {
+        outline-width: 0;
+    }
 `
 const LastName = styled.input`
     width: 40%;
     margin: 1rem;
     padding: .5rem;
     border: 4px dotted #e5c027;
+    &:focus {
+        outline-width: 0;
+    }
 `
 const Email = styled.input`
     width: 40%;
     margin: 1rem;
     padding: .5rem;
     border: 4px dotted #458962;
+    &:focus {
+        outline-width: 0;
+    }
 `
 const Password = styled.input`
     width: 40%;
     margin: 1rem;
     padding: .5rem;
     border: 4px dotted #125592;
+    &:focus {
+        outline-width: 0;
+    }
 `
 
 // containers for options about marketplace and seller/buyer
 const OptionCont = styled.div`
     width: 40%;
+    
 
 `
 
-const OptionName = styled.p``
+const OptionName = styled.p`
+`
 
 // dropdown menu with locations
 const MarketLocation = styled.select`
-    width: 30%;
+    width: 40%;
+    display: block;
+	font-size: 0.9rem;
+	font-weight: 700;
+	color: #444;
+	line-height: 1.3;
+	padding: .6em 1.4em .5em .8em;
+	max-width: 100%;
+	box-sizing: border-box;
+	margin: 0;
+	border: 1px solid #aaa;
+	box-shadow: 0 1px 0 1px rgba(0,0,0,.04);
+	border-radius: .5em;
+	-moz-appearance: none;
+	-webkit-appearance: none;
+	appearance: none;
+	background-color: #fff;
+	background-image: url('data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%23007CB2%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E'),
+	  linear-gradient(to bottom, #ffffff 0%,#e5e5e5 100%);
+	background-repeat: no-repeat, repeat;
+	background-position: right .7em top 50%, 0 0;
+	background-size: .65em auto, 100%;
+
 `
 
 // radios for type of user - seller, buyer, or both
-const UserType = styled.input``
-const UserLabel = styled.label``
+const UserType = styled.input`
+
+opacity: 0;
+position: fixed;
+width:0;
+
+:checked + label {
+    background-color:#c35b48;
+    border-color: #2b2926;
+}
+`
+const UserLabel = styled.label`
+
+background-color: #ddd;
+padding: 10px 20px;
+margin: 2px;
+border: 2px solid #444;
+border-radius: 4px;
+
+`
 
 const ButtonContainer = styled.div`
 display: flex;
@@ -120,12 +177,15 @@ const SignUpButton = styled.button`
     color:black;
     text-align:center;
     transition: all 0.2s;
+    font-size:1rem;
+
+    
 
 &:hover {
     color:white;
     background-color: grey;
+    transition: 1s;
 }
-
 `
 
 export default function SignupPage() {
@@ -183,7 +243,7 @@ export default function SignupPage() {
                             name="market_location"
                         >
                             <option value="Select">
-                                Select
+                                Select Location
                             </option>
                             <option value="Burundi">
                                 Burundi
