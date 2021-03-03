78f6209085f0855fce681f523d08a2a80173f26f
Ironipad64 <Ironipad64@gmail.com>
2021-03-03 10:54:52 -0800

    Routing

--- src/utils/ProtectedRoute.js
+++ src/utils/ProtectedRoute.js
@@ -3,15 +3,14 @@ import { Route, Redirect } from 'react-router-dom';
 
 // This is where you protect routes so users need a token to access
 
-const ProtectedRoute = ({ component:Component, ...rest}) => {
+const ProtectedRoute = ({ component: Component, ...rest }) => {
 
-  return(<Route
-    {...rest}
-    render={(props) => {
-      if(localStorage.getItem('token')) {
-        return <Component {...props}/>; //returns to components if it gets the token
+  return (<Route {...rest} render={
+    (props) => {
+      if (localStorage.getItem('token')) {
+        return <Component {...props} />; //returns to components if it gets the token
       } else {
-        return <Redirect to='/login'/>;  //redirects to login if it can't find a token
+        return <Redirect to='/login' />;  //redirects to login if it can't find a token
       }
     }}
   />)
