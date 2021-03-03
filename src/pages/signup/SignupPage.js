1e26c474b02a0808e6d1d46c949b8513b90505d3
Weston Woodard <westonwoodard28@gmail.com>
2021-03-03 15:42:56 -0700

    fixed errors

--- src/pages/signup/SignupPage.js
+++ src/pages/signup/SignupPage.js
@@ -23,12 +23,7 @@ export default function SignupPage() {
 
     const history = useHistory()// allows for movement to profile page upon form submit
 
-
-    const goToProfile = () => {
-        history.push('/profile')
-    }
-
-    const handleChange = (e) => {
+    const handleChange = (e) => {// updates form when events occur on page
         setForm({
             ...form,
             [e.target.name]: e.target.value
@@ -44,7 +39,6 @@ export default function SignupPage() {
                 console.log(res.data);
                 localStorage.setItem("token", res.data.access_token);
                 history.push("/profile")
-                // window.location.href = '/profile';
             })
             .catch((err) => {
                 console.log(err)
@@ -70,12 +64,6 @@ export default function SignupPage() {
                         onChange={handleChange}
                         value={form.username}
                     />
-                    {/* 
-                    <LastName
-                        name='last_name'
-                        type='text'
-                        placeholder='last name'
-                    /> */}
 
                     <Email
                         name='primaryemail'
@@ -92,38 +80,6 @@ export default function SignupPage() {
                         onChange={handleChange}
                         value={form.password}
                     />
-
-                    {/* <OptionCont>
-                        <OptionName>Main marketplace:</OptionName>
-                    
-
-                        {/* <MarketLocation
-                            name="market_location"
-                        >
-                            <option value="Select">
-                                Select Location
-                            </option>
-                            <option value="Burundi">
-                                Burundi
-                            </option>
-                            <option value="Kenya">
-                                Kenya
-                            </option>
-                            <option value="Rwanda">
-                                Rwanda
-                            </option>
-                            <option value="South Sudan">
-                                South Sudan
-                            </option>
-                            <option value="Tanzania">
-                                Tanzania
-                            </option>
-                            <option value="Uganda">
-                                Uganda
-                            </option>
-                        </MarketLocation> 
-
-                        </OptionCont>  */ }
 
                     <OptionCont onChange={handleChange} value={form.role}>
                         <OptionName>I am a:</OptionName>
@@ -149,27 +105,11 @@ export default function SignupPage() {
                         >
                             buyer
                         </UserLabel>
-                        {/* <UserType
-                            name='user_type'
-                            type='radio'
-                            id='both'
-                            value='both'
-                        />
-                        <UserLabel
-                            for='both'
-                        >
-                            both
-                        </UserLabel>
-                             */}
-                    </OptionCont>
-                    
+                    </OptionCont>                
                 </SignUpForm>
                 <ButtonContainer>
                     <SignUpButton onClick={signup} disabled={!form.username || !form.primaryemail || !form.password || !form.role}>sign up!</SignUpButton>
                 </ButtonContainer>
-
-                </SignUpForm>
-
             </SignUpBox>
         </Page>
     )
@@ -196,7 +136,6 @@ const kf = keyframes`//allows for fade in
 const Page = styled.div`
 font-family:'Roboto',sans-serif;
 `
-
 
 // banner
 const HeaderImg = styled.img`
@@ -248,6 +187,7 @@ const SignUpForm = styled.form`
 
     justify-content: space-evenly;  
 `
+
 //username
 const Username = styled.input`
     width: 40%;
@@ -258,6 +198,7 @@ const Username = styled.input`
         outline-width: 0;
     }
 `
+
 //email
 const Email = styled.input`
     width: 40%;
@@ -298,31 +239,6 @@ const OptionCont = styled.div`
 const OptionName = styled.p`
 `
 
-// dropdown menu with locations
-const MarketLocation = styled.select`
-    width: 40%;
-    display: block;
-	font-size: 0.9rem;
-	font-weight: 700;
-	color: #444;
-	line-height: 1.3;
-	padding: .6em 1.4em .5em .8em;
-	max-width: 100%;
-	box-sizing: border-box;
-	margin: 0;
-	border: 1px solid #aaa;
-	box-shadow: 0 1px 0 1px rgba(0,0,0,.04);
-	border-radius: .5em;
-	appearance: none; // hides actual drop down arrow
-	background-color: #fff;
-	background-image: url('data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%23007CB2%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E'),
-	  linear-gradient(to bottom, #ffffff 0%,#e5e5e5 100%);
-	background-repeat: no-repeat, repeat;
-	background-position: right .7em top 50%, 0 0;
-	background-size: .65em auto, 100%;
-
-`
-
 // radios for type of user - seller, buyer, or both
 const UserType = styled.input`
 //hides actual radio buttons
@@ -346,6 +262,7 @@ margin: 2px;
 border: 2px solid #444;
 border-radius: 4px;
 `
+
 //sign up button container
 const ButtonContainer = styled.div`
 display: flex;
