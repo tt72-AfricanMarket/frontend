e5a23dff858693ec6f4682489c920b32c08b5348
James <JamesWhite1911@protonmail.com>
2021-03-03 19:34:05 -0500

    edit items wip

--- src/pages/profile/UpdateForm.js
+++ src/pages/profile/UpdateForm.js
@@ -33,7 +33,7 @@ const UpdateForm = props => {
 
     useEffect(() => {
         axios
-            .get('https://african-marketplace-tt72.herokuapp.com/category/all')
+            .get(`https://african-marketplace-tt72.herokuapp.com/products/product/${id}`)
             .then(res => setItem(res.data))
             .catch(err => console.log(err))
     }, []);
@@ -41,9 +41,77 @@ const UpdateForm = props => {
     const changeHandler = e => {
         e.persist();
         let value = e.target.value;
+
         setItem({
             ...item,
-            [ev.target.name]: value
+            [e.target.name]: value
         });
     };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        axios
+        .put(`https://african-marketplace-tt72.herokuapp.com/products/product/${id}`, item)
+        .then(res => {
+          props.setItems(res.data)
+          props.history.push(`/profile`)
+        })
+        .catch(err => console.log(err))
+      };
+
+      return (
+        <div>
+          <h2>Update Item</h2>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              name="name"
+              onChange={changeHandler}
+              placeholder="name"
+              value={item.name}
+            />
+            <div className="baseline" />
+    
+            <input
+              type="number"
+              name="price"
+              onChange={changeHandler}
+              placeholder="Price"
+              value={item.price}
+            />
+            <div className="baseline" />
+    
+            <input
+              type="string"
+              name="imageUrl"
+              onChange={changeHandler}
+              placeholder="Image"
+              value={item.imageUrl}
+            />
+            <div className="baseline" />
+    
+            <input
+              type="string"
+              name="description"
+              onChange={changeHandler}
+              placeholder="Description"
+              value={item.description}
+            />
+            <div className="baseline" />
+    
+            <input
+              type="string"
+              name="shipping"
+              onChange={changeHandler}
+              placeholder="Shipping"
+              value={item.shipping}
+            />
+            <div className="baseline" />
+    
+            <button className="md-button form-button">Update</button>
+          </form>
+        </div>
+      );
 }
+
+export default UpdateForm;
